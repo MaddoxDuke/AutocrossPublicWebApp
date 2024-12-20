a0561fe540f40bc1f631f2a34c5f5fa60d289839
Maddox Duke <Maddoxduke911@Gmail.com>
2024-12-20 12:44:29 -0600

    Add project files.

---
+++ Models/ReadingModel.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+
+namespace AutocrossPublicWebApp.Models
+{
+    public class ReadingModel
+    {
+
+        public string Name { get; set; }
+        public HtmlDocument[] SelectedDocs { get; set; }
+        public int DocSize { get; set; }
+        public int Year { get; set; }
+        public int[] TrNthChild { get; set; }
+        public bool paxRaw { get; set; }
+    }
+}
